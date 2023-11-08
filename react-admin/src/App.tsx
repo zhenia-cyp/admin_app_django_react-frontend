03419b666840b52c03680ee7498e3d7614d053f1
zhenia <zhenia@ubuntu20.04>
2023-11-08 18:32:11 +0200

    Created routes for the Dashboard and Users components

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import './App.css';
-import Nav from "./components/Nav";
-import Menu from "./components/Menu";
-import Dashboard from "./components/Dashboard";
+import Nav from "./secure/components/Nav";
+import Menu from "./secure/components/Menu";
+import Dashboard from "./secure/Dashboard";
+import Users from "./secure/Users";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-
       <Nav/>
         <div className="container-fluid">
           <div className="row">
             <Menu/>
             <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-              <Dashboard/>
-
+               <BrowserRouter>
+                 <Routes>
+                  <Route path="/" element={<Dashboard/>}/>
+                  <Route path="/users" element={<Users/>}/>
+                 </Routes>
+               </BrowserRouter>
             </main>
           </div>
         </div>
---
+++ react-admin/src/secure/Users.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Users = () => (
+    <p>Users works!</p>
+)
+
+export default Users;

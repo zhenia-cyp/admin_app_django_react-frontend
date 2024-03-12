03e9e1e16f2eeb9030a64d7f4fe19894e68f1029
zhenia <zhenia@ubuntu20.04>
2024-03-12 09:58:10 +0200

    Added a function handleCsv to the order component converts order data to csv file

--- react-admin/src/secure/orders/Orders.tsx
+++ react-admin/src/secure/orders/Orders.tsx
@@ -16,9 +16,6 @@ class Orders extends Component {
 
     componentDidMount = async () =>  {
         const response = await axios.get(`orders/get/orders/?page=${this.page}`);
-        console.log('orders:',response);
-
-        console.log('newOrders:', this.newOrders);
         this.setState({
             orders: response.data.data
         })
@@ -32,50 +29,64 @@ class Orders extends Component {
 
     }
 
+    handleCsv = async () => {
+        const response = await axios.get('orders/export/csv/', {responseType: 'blob'});
+        const blob = new Blob([response.data], {type: 'text/csv'});
+        const downloadUrl = window.URL.createObjectURL(response.data);
+        const link = document.createElement('a');
+        link.href = downloadUrl;
+        link.download = 'orders.csv';
+        link.click();
+    }
     render(){
        return (
            <Wrapper>
-               <div className="table-responsive">
-                   <table className="table table-striped table-sm">
-                       <thead>
-                       <tr>
-                           <th scope="col">#</th>
-                           <th scope="col">Name</th>
-                           <th scope="col">Email</th>
-                           <th scope="col">Total</th>
-                           <th scope="col">Action</th>
-                       </tr>
-                       </thead>
-                       <tbody>
+               <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+                   <div className="btn-toolbar mb-2 mb-md-0">
+                       <a onClick={this.handleCsv} className="btn btn-sm btn-outline-secondary">export to csv</a>
+                   </div>
+                   </div>
+                   <div className="table-responsive">
+                       <table className="table table-striped table-sm">
+                           <thead>
+                           <tr>
+                               <th scope="col">#</th>
+                               <th scope="col">Name</th>
+                               <th scope="col">Email</th>
+                               <th scope="col">Total</th>
+                               <th scope="col">Action</th>
+                           </tr>
+                           </thead>
+                           <tbody>
 
-                       {this.state.orders.map(
-                           (order: Order,index) => {
-                               return (
-                                   <tr>
-                                       <td>{order.order_num}</td>
-                                       <td>{order.first_name} {order.last_name}</td>
-                                       <td>{order.email}</td>
-                                       <td>{order.total}</td>
-                                       <td>
-                                           <div className="btn-group mr-2">
-                                               <Link to={`/orders/${order.id}/`}
-                                                     className="btn btn-sm btn-outline-secondary">View</Link>
+                           {this.state.orders.map(
+                               (order: Order, index) => {
+                                   return (
+                                       <tr>
+                                           <td>{order.order_num}</td>
+                                           <td>{order.first_name} {order.last_name}</td>
+                                           <td>{order.email}</td>
+                                           <td>{order.total}</td>
+                                           <td>
+                                               <div className="btn-group mr-2">
+                                                   <Link to={`/orders/${order.id}/`}
+                                                         className="btn btn-sm btn-outline-secondary">View</Link>
 
-                                           </div>
-                                       </td>
-                                   </tr>
-                               )
+                                               </div>
+                                           </td>
+                                       </tr>
+                                   )
 
-                           }
-                       )}
+                               }
+                           )}
 
-                       </tbody>
-                   </table>
-               </div>
-               <Paginator lastPage={this.last_page} handlePageChange={this.handlePageChange}/>
+                           </tbody>
+                       </table>
+                   </div>
+                   <Paginator lastPage={this.last_page} handlePageChange={this.handlePageChange}/>
            </Wrapper>
-       )
+    )
     }
-}
+    }
 
-export default Orders;
+    export default Orders;
